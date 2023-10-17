a80f8daba8fdd189bb3bb953759ba748b87127cf
Tokkiory <teamchampionsvietnam.com>
2023-10-17 20:35:35 +0700

    Circle Struct Area calculation

--- Problems/CircleStructArea.cpp
+++ Problems/CircleStructArea.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cmath>
+#include <iomanip>
 
 struct Circle
 {
@@ -26,18 +27,16 @@ double calculate(double x, double y)
     return x * pow(y, 2.0) * M_PI;
 }
 
-void result(Circle r)
-{
-    std::cout << r.area;
-}
-
 int main()
 {
+    //Call struct 
     Circle info = getNumber();
 
+    //Manipulate digit number
+    std::cout << std::fixed << std::showpoint << std::setprecision(2);
+
     double calculated = calculate(info.diameter, info.radius);
-    
-    result(info);
+    std::cout << calculated;
 
     return 0;
 }
